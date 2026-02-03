d2ed714b4b9c7cec23295b692175f548c43598be
Eric Doering <eric.doering@EDOERING.local>
2026-02-03 17:07:43 -0500

    landing page schema added

--- studio-art-gallery/schemaTypes/index.ts
+++ studio-art-gallery/schemaTypes/index.ts
@@ -1,4 +1,5 @@
 import { artwork } from "./artwork";
 import { contact } from "./contact";
+import { landing } from "./landing";
 
-export const schemaTypes = [artwork, contact]
+export const schemaTypes = [artwork, contact, landing]
