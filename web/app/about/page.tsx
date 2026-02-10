6457f698664eb6b99a204a056e4c8e2342bd3d74
Eric Doering <ericbenjamindoering@gmail.com>
2026-02-09 19:33:43 -0500

    navbar added to all pages

--- web/app/about/page.tsx
+++ web/app/about/page.tsx
@@ -1,6 +1,9 @@
+import Navbar from '@/components/Navbar';
+
 export default function AboutPage() {
     return (
       <main>
+        <Navbar />
         <h1>About</h1>
         <p>This is the About page</p>
       </main>
--- web/app/contact/page.tsx
+++ web/app/contact/page.tsx
@@ -1,6 +1,9 @@
+import Navbar from '@/components/Navbar';
+
 export default function ContactPage() {
     return (
       <main>
+        <Navbar />
         <h1>Contact</h1>
         <p>This is the Contact page</p>
       </main>
