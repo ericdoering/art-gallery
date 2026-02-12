d42227c019004329678982cc6a3cf0cec5d4ae45
Eric Doering <ericbenjamindoering@gmail.com>
2026-02-11 23:47:03 -0500

    about schema created

--- studio-art-gallery/schemaTypes/index.ts
+++ studio-art-gallery/schemaTypes/index.ts
@@ -1,5 +1,6 @@
 import { artwork } from "./artwork";
 import { contact } from "./contact";
 import { landing } from "./landing";
+import { about } from "./about";
 
-export const schemaTypes = [artwork, contact, landing]
+export const schemaTypes = [artwork, contact, landing, about]
